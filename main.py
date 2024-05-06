c2e78e5916407d7522398b76c46e92addae74bbd
Jonathanace <jonathanace@gmail.com>
2024-05-05 23:02:24 -0700

    refactor solve function

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from puzzle import *
 from utils import * 
+import timeit
 
 # Helper function to solve a 
 def solve(grid, criteria):
     init_state = State(np.array(grid))
     puzzle = Puzzle(init_state, criteria=criteria)
     start = time.time()
-    path = puzzle.solve()
+    solution = puzzle.solve()
     end = time.time()
     duration = end - start
-    return {'path': path,
-            'duration': duration}
+    return solution
 
 # Function to solve for all starting states and with all criteria
 def solve_all():
@@ -27,20 +27,11 @@ def solve_all():
 
 
 
-# Define premade initial states
-start_states = {
-    'trivial': trivial,
-    'very_easy': very_easy,
-    'easy': easy,
-    'doable': doable,
-    'oh_boy': oh_boy
-}
+if __name__ == "__main__":
+    solution = solve([[1,0,3],[4,2,6],[7,5,8]], misplaced_tile)
+    for step in solution['path']:
+        print(step)
 
-criterium = {
-    'uniform_cost': uniform_cost,
-    'misplaced_tile': misplaced_tile,
-    'euclidean_distance': euclidean_distance
-}
-
-solve(doable, euclidean_distance)
-solve_all()
+    print(f'Nodes Expanded (Time): {solution['time']}')
+    print(f'Max Frontier Size (space): {solution['space']}')
+    # print(solve(doable, misplaced_tile))
